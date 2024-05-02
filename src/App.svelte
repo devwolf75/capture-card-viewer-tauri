a4f3312ee664ddb1d71b3c81367e68675af0f8f8
devwolf75 <dev.wolf75@gmail.com>
2024-05-02 12:59:26 -0600

    Add double click for full screen.
    Default to dark mode to prevent burning retinas.

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,22 @@
 <script>
   import { onMount } from "svelte";
+  import { appWindow } from "@tauri-apps/api/window";
 
   let videoValue = "null";
   let audioValue = "null";
 
   let videoElement;
   let audioElement;
+  let errorElement;
   let aspectRatioSelect;
 
   let videoDevices = [];
   let audioDevices = [];
 
-  onMount(async () => {
+  onMount(() => {
     videoElement = document.querySelector("video");
     audioElement = document.querySelector("audio");
+    errorElement = document.getElementById("error-element");
     aspectRatioSelect = document.getElementById("aspectRatioSelect");
 
     getSources();
@@ -21,6 +24,7 @@
 
   // Get the available video or audio sources
   async function getSources() {
+    // @ts-ignore
     await navigator.getUserMedia(
       { audio: true, video: true },
       () => {
@@ -33,11 +37,12 @@
           );
         });
       },
-      () => {
+      (err) => {
         errorMsg(
           "Permissions have not been granted to use your camera and " +
             "microphone, you need to allow the page access to your devices in " +
-            "order for the demo to work."
+            "order for the application to work." +
+            `${err}`
         );
       }
     );
@@ -61,8 +66,17 @@
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     try {
       if (sourceType === "video") {
-        videoElement.srcObject = stream;
-        videoElement.play();
+        if (source !== "null") {
+          videoElement.srcObject = stream;
+          videoElement.onpause = () => {
+            videoElement.play();
+          }; // Prevent accidental pause.
+          videoElement.play();
+        } else {
+          videoElement.pause();
+          videoElement.removeAttribute("src");
+          videoElement.load();
+        }
       }
       if (sourceType === "audio") {
         audioElement.srcObject = stream;
@@ -74,17 +88,36 @@
   }
 
   function changeAspectRatio(ratio) {
-    let selection = aspectRatioSelect.options[aspectRatioSelect.selectedIndex].value;
+    let selection =
+      aspectRatioSelect.options[aspectRatioSelect.selectedIndex].value;
     let currentRatio = videoElement.videoWidth / videoElement.videoHeight;
-    let targetRatio = eval(selection.replace(":", "/"));
+    let values = selection.split(":");
+    let targetRatio = values[0] / values[1];
     let adjustmentRatio = targetRatio / currentRatio;
     videoElement.style.webkitTransform = `scaleX(${adjustmentRatio})`;
     videoElement.style.transform = `scaleX(${adjustmentRatio})`;
   }
 
+  function makeFullScreen() {
+    if (!document.fullscreenElement && videoValue !== "null") {
+      videoElement.requestFullscreen();
+      videoElement.removeAttribute("controls");
+      appWindow.setFullscreen(true);
+    } else if (document.exitFullscreen) {
+      appWindow.setFullscreen(false);
+      document.exitFullscreen();
+    } else {
+      console.error("Fullscreen API is not supported");
+    }
+  }
+
   function errorMsg(msg, error) {
-    errorElement.innerHTML += "<p>" + msg + "</p>";
-    if (typeof error !== "undefined") {
+    try {
+      errorElement.innerHTML += "<p>" + msg + "</p>";
+      if (typeof error !== "undefined") {
+        console.error(error);
+      }
+    } catch (exception) {
       console.error(error);
     }
   }
@@ -93,57 +126,68 @@
 <svelte:head>
   <meta charset="UTF-8" />
   <title>Capture Card Viewer</title>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
-  />
 </svelte:head>
 
 <div class="content">
   <div class="main-section">
-    <video src="" />
+    <video src="" on:dblclick={(event) => makeFullScreen()} />
     <audio src="" />
   </div>
 
-  <div class="button-section">
-    <select
-      bind:value={videoValue}
-      on:change={(event) => selectSource(event.target.value, "video")}
+  <div class="button-section" id="button-section">
+    <div
+      class="select is-rounded {videoDevices.length === 0 ? 'is-loading' : ''}"
     >
-      <option value="null">Video</option>
-      {#each videoDevices as video}
-        <option value={video.deviceId}>
-          {video.label}
-        </option>
-      {/each}
-    </select>
-    <select
-      bind:value={audioValue}
-      on:change={(event) => selectSource(event.target.value, "audio")}
+      <select
+        bind:value={videoValue}
+        on:change={(event) => selectSource(event.target.value, "video")}
+      >
+        <option value="null">Video</option>
+        {#each videoDevices as video}
+          <option value={video.deviceId}>
+            {video.label}
+          </option>
+        {/each}
+      </select>
+    </div>
+    <div
+      class="select is-rounded {audioDevices.length === 0 ? 'is-loading' : ''}"
     >
-      <option value="null">Audio</option>
-      {#each audioDevices as audio}
-        <option value={audio.deviceId}>
-          {audio.label}
-        </option>
-      {/each}
-    </select>
-    <select id="aspectRatioSelect" on:change={(event) => changeAspectRatio(event.target.value)}>
-      <option value="null">Aspect Ratio</option>
-      <option value="1:1">1:1</option>
-      <option value="4:3">4:3</option>
-      <option value="16:9">16:9</option>
-      <option value="16:10">16:10</option>
-      <option value="1.85:1">1.85:1</option>
-      <option value="2.35:1">2.35:1</option>
-      <option value="9:16">Vertical (9:16)</option>
-    </select>
+      <select
+        bind:value={audioValue}
+        on:change={(event) => selectSource(event.target.value, "audio")}
+      >
+        <option value="null">Audio</option>
+        {#each audioDevices as audio}
+          <option value={audio.deviceId}>
+            {audio.label}
+          </option>
+        {/each}
+      </select>
+    </div>
+    <div class="select is-rounded">
+      <select
+        id="aspectRatioSelect"
+        on:change={(event) => changeAspectRatio(event.target.value)}
+      >
+        <option value="null">Aspect Ratio</option>
+        <option value="1:1">1:1</option>
+        <option value="4:3">4:3</option>
+        <option value="16:9">16:9</option>
+        <option value="16:10">16:10</option>
+        <option value="1.85:1">1.85:1</option>
+        <option value="2.35:1">2.35:1</option>
+        <option value="9:16">Vertical (9:16)</option>
+      </select>
+    </div>
   </div>
 
-  <div class="error-section" />
+  <div class="error-section" id="error-section"></div>
 </div>
 
-<style type="text/scss">
+<style lang="scss">
+  @import "https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css";
+
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
       Helvetica, Arial, sans-serif;
@@ -161,7 +205,7 @@
         display: block;
         position: relative;
         top: -100px;
-        background-color: #fff;
+        background-color: transparent;
       }
     }
   }
@@ -186,5 +230,9 @@
       top: -100px;
       background-color: #fff0;
     }
+  }
+
+  video::-webkit-media-controls {
+    display: none;
   }
 </style>
